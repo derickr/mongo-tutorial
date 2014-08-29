5eee26b595f871f0802c00cfdd365132a0987f58
Derick Rethans <github@derickrethans.nl>
2014-08-29 16:25:45 +0100

    Add finding script for exercise 2

---
+++ 02-find.php
@@ -0,0 +1,21 @@
+<?php
+$m = new MongoClient;
+$d = $m->tutorial;
+$c = $d->beer;
+
+$document = $c->findOne( [ 'beer_name' => 'USA Hells' ] );
+echo date_create( $document->created_at )->format( 'Y-m-d' ), "\n\n";
+
+$cursor = $c->find(
+	[ 'rating_score' => [ '$gte' => '4.5' ] ],
+	[ 'beer_name' => 1, 'rating_score' => 1, 'created_at' => 1 ] 
+);
+$cursor->sort( [ 'rating_score' => -1, 'created_at' => 1 ] );
+
+foreach ( $cursor as $document )
+{
+	echo $document['beer_name'], "\n", 
+		$document['rating_score'],  ' - ',
+		$document['created_at'], "\n";
+}
+?>
