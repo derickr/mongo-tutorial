0822bb60e5ebc52c422afa0df1fa04237c3225ce
Derick Rethans <github@derickrethans.nl>
2014-09-01 21:10:13 +0100

    Fixed the script to use proper data types.

--- 01-import.php
+++ 01-import.php
@@ -7,5 +7,10 @@ $c->drop();
 foreach ( file( 'untappd.json' ) as $checkin )
 {
 	$document = json_decode( $checkin );
+	foreach ( [ 'beer_abv', 'beer_ibu', 'venue_lat', 'venue_lng', 'rating_score' ] as $field )
+	{
+		$document->$field = (float) $document->$field;
+	}
+	$document->created_at = new MongoDate( strtotime( $document->created_at ) );
 	$c->insert( $document );
 }
--- 02-find.php
+++ 02-find.php
@@ -7,7 +7,7 @@ $document = $c->findOne( [ 'beer_name' => 'USA Hells' ] );
 echo date_create( $document->created_at )->format( 'Y-m-d' ), "\n\n";
 
 $cursor = $c->find(
-	[ 'rating_score' => [ '$gte' => '4.5' ] ],
+	[ 'rating_score' => [ '$gte' => 4.5 ] ],
 	[ 'beer_name' => 1, 'rating_score' => 1, 'created_at' => 1 ] 
 );
 $cursor->sort( [ 'rating_score' => -1, 'created_at' => 1 ] );
@@ -16,6 +16,6 @@ foreach ( $cursor as $document )
 {
 	echo $document['beer_name'], "\n", 
 		$document['rating_score'],  ' - ',
-		$document['created_at'], "\n";
+		date_create( "@{$document['created_at']->sec}" )->format( 'Y-m-d' ), "\n";
 }
 ?>
